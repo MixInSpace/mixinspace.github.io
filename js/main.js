60256843d261e43335763c9bd1178ed041ce23e8
MixInSpace <mi.maruas@yandex.ru>
2023-05-05 20:03:37 +0800

    Finaly

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
-var backgroundColor = 'rgba(255,255,255,1)';
+var backgroundColor = 'rgba(35, 25, 66, 1)';
+document.body.style.backgroundColor = backgroundColor
 
 document.addEventListener('mousemove', (ev) => {
-  let nSizeOfPart = document.documentElement.offsetWidth / 100;
-  backgroundColor = 'rgba(' + ev.clientX / nSizeOfPart + ',' + ev.clientY / nSizeOfPart + ',50,1)';
+  let xSizeOfPart = document.documentElement.offsetWidth / 100;
+  let ySizeOfPart = document.documentElement.offsetHeight / 100;
+  backgroundColor = 'rgba(' + (ev.clientX / xSizeOfPart) % 100 + ',' + (ev.clientY / ySizeOfPart) % 100 + ',50,1)';
   document.body.style.backgroundColor = backgroundColor
 });
 
